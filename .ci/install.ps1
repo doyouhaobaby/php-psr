5f3639f0d9b09225eefd8089e8f0e22f03607ee3
John Boehr <jbboehr@gmail.com>
2017-12-20 18:32:38 -0800

    null

--- .ci/install.ps1
+++ .ci/install.ps1
@@ -1,8 +1,6 @@
 Set-PSDebug -Trace 1
 . "${env:APPVEYOR_BUILD_FOLDER}\.ci\env.ps1"
 
-cinst wget pscx
-
 (new-object net.webclient).DownloadFile("https://github.com/OSTC/php-sdk-binary-tools/archive/${env:PHP_SDK_BINARY_TOOLS_VER}.zip", "C:\projects\${env:PHP_SDK_BINARY_TOOLS_VER}.zip")
 7z x -y "C:\projects\${env:PHP_SDK_BINARY_TOOLS_VER}.zip" -oC:\projects
 Move-Item C:\projects\php-sdk-binary-tools-${env:PHP_SDK_BINARY_TOOLS_VER} C:\projects\php-sdk
